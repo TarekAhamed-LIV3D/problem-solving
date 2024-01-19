b226dc38a61e135a53bd920a8937e7403f997750
alpha <18-36744-1@student.aiub.edu>
2024-01-18 23:12:04 -0800

    problem 1302

---
+++ deepest-leaves-sum.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def deepestLeavesSum(root: TreeNode) -> int:
+    if not root:
+        return 0
+
+    queue = [root]
+    while queue:
+        level_sum = 0
+        level_size = len(queue)
+
+        for _ in range(level_size):
+            node = queue.pop(0)
+            level_sum += node.val
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+                
+    return level_sum
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(6)
+root.left.left.left = TreeNode(7)
+root.right.right.right = TreeNode(8)
+
+result = deepestLeavesSum(root)
+print(result)
