d73adbbfb520cff6cce0f3988f36b6e703a5b6a9
alpha <18-36744-1@student.aiub.edu>
2024-01-23 23:18:42 -0800

    Problem 204

---
+++ count-primes.py
@@ -0,0 +1,15 @@
+def countPrimes(n):
+    if n <= 2:
+        return 0
+
+    is_prime = [True] * n
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            is_prime[i*i:n:i] = [False] * len(is_prime[i*i:n:i])
+
+    return sum(is_prime)
+
+n = 100
+print(countPrimes(n))
