4c6299fda550d7ef4c5fac7c830ef4b95d9e26d5
alpha <18-36744-1@student.aiub.edu>
2024-01-23 22:58:48 -0800

    Problem 75

---
+++ sort-colors.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
+solution = Solution()
+colors = [2,0,2,1,1,0,1,2,1]
+
+solution.sortColors(colors)
+print(colors)
