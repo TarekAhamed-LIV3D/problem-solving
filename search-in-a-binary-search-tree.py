7333821e40f3bfdf1d2698647121bcc531216740
AhamedTheMorningStar <18-36744-1@student.aiub.edu>
2024-04-29 23:58:36 -0700

    Leetcode easy problem solve 700

--- search-in-a-binary-search-tree.py
+++ search-in-a-binary-search-tree.py
@@ -2,3 +2,5 @@
 # Find the node in the BST that the node's value equals val and return the subtree rooted with that node. 
 # If such a node does not exist, return null.
 
+def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    
